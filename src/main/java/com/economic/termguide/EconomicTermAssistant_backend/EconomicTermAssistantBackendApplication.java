d15fe3c4e488df00b3edac87348790d21a9c294e
hs98012 <hs98012@daum.net>
2023-12-30 00:25:38 +0900

    Initial commit

---
+++ src/main/java/com/economic/termguide/EconomicTermAssistant_backend/EconomicTermAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.economic.termguide.EconomicTermAssistant_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomicTermAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomicTermAssistantBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/economic/termguide/EconomicTermAssistant_backend/EconomicTermAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.economic.termguide.EconomicTermAssistant_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomicTermAssistantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
