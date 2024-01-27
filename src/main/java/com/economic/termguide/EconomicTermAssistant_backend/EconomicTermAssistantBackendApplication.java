86e44f53a7c5c3b3647409ec48808c08133ae3b2
hs98012 <hs98012@daum.net>
2024-01-28 03:40:41 +0900

    [feat] ec2 설정 맞추기

--- src/main/java/com/economic/termguide/EconomicTermAssistant_backend/EconomicTermAssistantBackendApplication.java
+++ src/main/java/com/economic/termguide/EconomicTermAssistant_backend/EconomicTermAssistantBackendApplication.java
@@ -2,12 +2,18 @@ package com.economic.termguide.EconomicTermAssistant_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EconomicTermAssistantBackendApplication {
+public class EconomicTermAssistantBackendApplication extends org.springframework.boot.web.servlet.support.SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EconomicTermAssistantBackendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EconomicTermAssistantBackendApplication.class);
 	}
 
 }
